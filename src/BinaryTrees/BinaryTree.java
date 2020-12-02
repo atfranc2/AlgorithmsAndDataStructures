316bb8cf37d2988981431bbe7012a1d60983715a
atfranc2 <atfranc2@ncsu.edu>
2020-12-01 19:50:34 -0500

    Scaffolded binary tree code

---
+++ src/BinaryTrees/BinaryTree.java
@@ -0,0 +1,21 @@
+package BinaryTrees;
+
+public class BinaryTree {
+    private Node root;
+
+
+
+    private class Node {
+        public int value;
+        public Node leftChild;
+        public Node rightChild;
+
+        public void insert(int value) {
+
+        }
+
+        public Node(int value) {
+            this.value = value;
+        }
+    }
+}
---
+++ src/Main.java
@@ -0,0 +1,7 @@
+import BinaryTrees.BinaryTree;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world");
+    }
+}
