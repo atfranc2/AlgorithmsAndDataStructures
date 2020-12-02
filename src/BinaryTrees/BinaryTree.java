043e941fe968264248b311539f1d5da7e21fa762
atfranc2 <atfranc2@ncsu.edu>
2020-12-01 21:01:49 -0500

    Added code to insert a value into the tree

--- src/BinaryTrees/BinaryTree.java
+++ src/BinaryTrees/BinaryTree.java
@@ -1,18 +1,65 @@
 package BinaryTrees;
 
+import java.security.PrivilegedAction;
+import java.sql.SQLOutput;
+
 public class BinaryTree {
     private Node root;
 
+    public BinaryTree(int value) {
+        root = new Node(value);
+    }
 
+    public void insert(int value) {
+        var current = root;
+        Node previousNode;
+
+        while (true) {
+            previousNode = current;
+            current = getChild(current, value);
+            if (current == null) break;
+        }
+
+        var newNode = new Node(value);
+        if (previousNode.value > value)
+            previousNode.leftChild = newNode;
+        else
+            previousNode.rightChild = newNode;
+
+    }
+
+    private Node getChild(Node currentNode, int incomingValue) {
+        return (currentNode.value > incomingValue) ? currentNode.leftChild : currentNode.rightChild;
+    }
+
+
+
+    private Node getRightTreeValues(Node node) {
+        if (node == null)
+            return null;
+        getLeftTreeValues(node.leftChild);
+        System.out.println(node.value);
+        return getRightTreeValues(node.rightChild);
+    }
+
+    private Node getLeftTreeValues(Node node) {
+        if (node == null)
+            return null;
+        getRightTreeValues(node.rightChild);
+        System.out.println(node.value);
+        return getLeftTreeValues(node.leftChild);
+    }
+
+    public void printTree() {
+        System.out.println(root.value);
+        getLeftTreeValues(root.leftChild);
+        getRightTreeValues(root.rightChild);
+    }
 
     private class Node {
         public int value;
         public Node leftChild;
         public Node rightChild;
-
-        public void insert(int value) {
-
-        }
 
         public Node(int value) {
             this.value = value;
--- src/Main.java
+++ src/Main.java
@@ -2,6 +2,15 @@ import BinaryTrees.BinaryTree;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world");
+        var btree = new BinaryTree(10);
+        btree.insert(5);
+        btree.insert(15);
+        btree.insert(6);
+        btree.insert(1);
+        btree.insert(8);
+        btree.insert(12);
+        btree.insert(18);
+        btree.insert(17);
+        btree.printTree();
     }
 }
